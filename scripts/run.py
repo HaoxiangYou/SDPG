8f738945eb918497a274b7ddffc1ba95a12d44fb
Haoxiang You <haoxiang.you@yale.edu>
2025-12-21 19:37:54 -0500

    minior updates and rename

--- scripts/run.py
+++ scripts/run.py
@@ -22,9 +22,9 @@ OmegaConf.register_new_resolver("compute_num_envs", compute_num_envs)
 
 
 def make_envs(config: DictConfig):
-    """Create environment based on task suite."""
-    task_suite = config.task.get("suite")
-    TaskSuite = getattr(envs, task_suite + "_env")
+    """Create environment based on task backend."""
+    task_backend = config.task.get("backend")
+    TaskSuite = getattr(envs, task_backend + "_env")
     return TaskSuite.make_envs(config)
 
 
