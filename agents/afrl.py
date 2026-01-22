acfb1b310647050615c539dd38ba5fea627b1e58
Haoxiang You <haoxiang.you@yale.edu>
2026-01-22 11:36:49 -0500

    fix typo

--- agents/afrl.py
+++ agents/afrl.py
@@ -5,12 +5,12 @@ import time
 
 import torch
 import torch.nn.functional as F
-import wandb
 from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig, OmegaConf
 from tensorboardX import SummaryWriter
 
 import models
+import wandb
 from utils.common_utils import TimeReport, make_envs, print_info
 from utils.statistic_utils import AverageMeter, RunningMeanStd
 from utils.tensor_utils import assign_row_intervals, compute_grad_norm
@@ -342,7 +342,7 @@ class AFRLRunner:
                 # TODO: currently assume the action is bounded by [-1, 1], and we step using tanh
                 obs, rewards, terminated, truncated, info = self.env.step(torch.tanh(actions), auto_reset=False)
                 # TODO: temporarily solutions, will consider which keys to use for actor and critic separation later and how to concatenate the observations later
-                obs = obs["previlaged_observations"]
+                obs = obs["privileged_observations"]
 
                 # Normalize the reward
                 raw_rewards = rewards.clone()
@@ -672,7 +672,7 @@ class AFRLRunner:
         states = self.env.get_states(env_ids=torch.arange(self.num_envs, device=self.device, dtype=torch.int32))
         obs = self.env.compute_observations(states=states)
         # TODO: temporarily solutions, will consider which keys to use for actor and critic separation later and how to concatenate the observations later
-        obs = obs["previlaged_observations"]
+        obs = obs["privileged_observations"]
 
         return obs, dones
 
@@ -684,7 +684,7 @@ class AFRLRunner:
         states = self.env.get_states(env_ids=torch.arange(self.num_envs, device=self.device, dtype=torch.int32))
         obs = self.env.compute_observations(states=states)
         # TODO: temporarily solutions, will consider which keys to use for actor and critic separation later and how to concatenate the observations later
-        obs = obs["previlaged_observations"]
+        obs = obs["privileged_observations"]
         return obs
 
     def reset_auxiliary_envs(self, env_ids: torch.Tensor):
