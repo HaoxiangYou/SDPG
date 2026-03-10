0d644fdb7b4fe24296c36c1226c914a9d7123016
YilangLiu <yilang.liu@yale.edu>
2026-03-10 16:51:27 -0400

    update training and evaluation scripts for rsl

--- hardware/go2/rsl/train_rsl_go2.py
+++ hardware/go2/rsl/train_rsl_go2.py
@@ -1,18 +1,17 @@
-import copy
-import os
-
 # from typing import Any, Dict, Tuple
+
 import genesis as gs
 import torch
+import wandb
 from rsl_rl.runners import OnPolicyRunner
 
 from envs.genesis_env.go2 import Go2
 
 env_name = "go2"
-num_envs = 20
+num_envs = 4096
 device = "cuda"
 sim_options = gs.options.SimOptions(dt=0.02, substeps=4)
-env_kwargs = {"show_viewer": True, "randomize_init": True}
+env_kwargs = {"show_viewer": False, "randomize_init": True}
 
 train_cfg = {
     "algorithm": {
@@ -39,39 +38,75 @@ train_cfg = {
     "runner": {
         "algorithm_class_name": "PPO",
         "checkpoint": -1,
-        "experiment_name": "test_genesis_env_rsl",
+        "experiment_name": "go2_rough",
         "load_run": -1,
         "log_interval": 1,
-        "max_iterations": 1,
+        "max_iterations": 5000,
         "num_steps_per_env": 24,
         "policy_class_name": "ActorCritic",
         "record_interval": 50,
         "resume": False,
         "resume_path": None,
-        "run_name": "",
+        "run_name": "go2_rsl",
         "runner_class_name": "runner_class_name",
         "save_interval": 100,
+        'record_interval': 50,
     },
     "runner_class_name": "OnPolicyRunner",
     "seed": 1,
 }
 
 domain_rand_options = {
-    "randomize_friction": False,
+    "randomize_friction": True,
     "friction_range": [0.2, 1.5],
-    "randomize_base_mass": False,
+    "randomize_base_mass": True,
     "added_mass_range": [-1.0, 3.0],
-    "randomize_com_displacement": False,
+    "push_robot": True,
+    "push_interval_s": 10,
+    "max_push_vel_xy": 1.0,
+    "randomize_com_displacement": True,
     "com_displacement_range": [-0.01, 0.01],
     "randomize_motor_strength": False,
     "motor_strength_range": [0.9, 1.1],
-    "randomize_motor_offset": False,
+    "randomize_motor_offset": True,
     "motor_offset_range": [-0.02, 0.02],
-    "randomize_kp_scale": False,
+    "randomize_kp_scale": True,
     "kp_scale_range": [0.8, 1.2],
-    "randomize_kd_scale": False,
+    "randomize_kd_scale": True,
     "kd_scale_range": [0.8, 1.2],
+    "use_terrain": True,
+    "obtain_terrain_info_around_feet": True,
+    "obtain_link_contact_states": True,
+    "contact_state_link_names": ["thigh", "calf", "foot", "base", "hip"],
+    "penalize_contacts_on": ["thigh", "calf", "base", "head", "hip"],
+    "terrain_cfg": {
+        "mesh_type": "heightfield",
+        "curriculum": True,
+        "selected": False,
+        "obtain_terrain_info_around_feet": True,
+        "measure_heights": True,
+        "measured_points_x": [-0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4],  # 9x9=81
+        "measured_points_y": [-0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4],
+        "border_size": 20.0,
+        "border_height": 1.0,
+        "terrain_length": 8.0,
+        "terrain_width": 8.0,
+        "platform_size": 4.0,
+        "num_rows": 10,  # number of terrain rows (levels)
+        "num_cols": 10,  # number of terrain cols (types)
+        "num_subterrains": 100,
+        "horizontal_scale": 0.1,  # [m] distance between height samples in x and y direction
+        "vertical_scale": 0.005,  # [m] distance between height samples in z direction
+        "static_friction": 1.0,  # coefficient of static friction of the terrain
+        "dynamic_friction": 1.0,  # coefficient of dynamic friction of the terrain
+        "restitution": 0.0,  # coefficient of restitution of the terrain
+        "max_init_terrain_level": 1,  # starting curriculum level
+        # terrain types: [smooth slope, rough slope, stairs up, stairs down, hurtle, stepping stones, gap, pit]
+        "terrain_proportions": [0.2, 0.1, 0.25, 0.25, 0.2],
+    },
 }
+
+log_dir = "logs/genesis_go2_rsl"
 
 
 class RSL_Wrapper(Go2):
@@ -89,14 +124,6 @@ class RSL_Wrapper(Go2):
             infos,
         )
 
-
-def export_policy_as_jit(actor_critic, path, name):
-    os.makedirs(path, exist_ok=True)
-    path = os.path.join(path, f"{name}.pt")
-    model = copy.deepcopy(actor_critic.actor).to("cpu")
-    traced_script_module = torch.jit.script(model)
-    traced_script_module.save(path)
-
 def main():
     env = RSL_Wrapper(
         num_envs=num_envs,
@@ -107,36 +134,11 @@ def main():
         **env_kwargs,
     )
 
-    log_dir = "logs/genesis_go2_rsl"
-    checkpooint = 5000
-    jit_ckpt_path = os.path.join(log_dir, "exported", "jit_model.pt")
-    if os.path.exists(jit_ckpt_path):
-        policy = torch.jit.load(jit_ckpt_path)
-        policy.to(device="cuda:0")
-    else:
-        runner = OnPolicyRunner(env, train_cfg, log_dir, device=gs.device)
+    runner = OnPolicyRunner(env, train_cfg, log_dir, device=gs.device)
 
-        resume_path = os.path.join(log_dir, f"model_{checkpooint}.pt")
-        runner.load(resume_path)
-        path = os.path.join(log_dir, "exported")
-        export_policy_as_jit(runner.alg.actor_critic, path, "jit_model")
-        policy = torch.jit.load(jit_ckpt_path)
-        policy.to(device="cuda:0")
+    wandb.init(project="genesis", name="genesis_go2_rsl", dir=log_dir, mode="online")
 
-    env.reset()
-    obs = env.get_observations()
-
-    with torch.no_grad():
-        stop = False
-        n_frames = 0
-        env.start_recording(record_internal=False)
-        while not stop:
-            actions = policy(obs)
-            obs, _, rews, dones, infos = env.step(actions)
-            n_frames += 1
-            if n_frames == 1000:
-                env.stop_recording(os.path.join(os.getcwd(), "video.mp4"))
-                exit()
+    runner.learn(num_learning_iterations=5000, init_at_random_ep_len=True)
 
 
 if __name__ == "__main__":
